87a358d8c88d37a0f57a02c399e93d1792c60626
Kamaldeen Alabi <myjavabootcamp@gmail.com>
2016-09-08 09:53:09 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# newSailsProject
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/MusiciansController.js
@@ -0,0 +1,11 @@
+/**
+ * MusiciansController
+ *
+ * @description :: Server-side logic for managing musicians
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
