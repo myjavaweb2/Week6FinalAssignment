0bbfa2d3e12512519fe4b996ca52fb834aaf8cf4
Kamaldeen Alabi <myjavabootcamp@gmail.com>
2016-09-08 11:05:43 -0400

    Added Schema

--- api/models/Musicians.js
+++ api/models/Musicians.js
@@ -1,14 +1,53 @@
 /**
  * Musicians.js
  *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @description :: This module will create the schema for the Musician API with the tables to store their records.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
 module.exports = {
 
   attributes: {
-
+  	schema: true,
+  	musicianID: {
+    	type: 'integer',
+    	required: true,
+    	unique: true,
+    	autoIncrement: true
+  },
+  	firstName: {
+	    type: 'string',
+	    required: true,
+	    minLength: 2
+  },
+  	lastName: {
+    	type: 'string',
+	    required: true,
+	    minLength: 2
+    },
+    typeOfMusic: {
+    	type: 'string',
+    	required: true
+    },
+    homeCity: {
+	    type: 'string',
+	    required: true,
+	    minLength: 2
+  },
+  	homeState: {
+    	type: 'string',
+	    required: true,
+	    minLength: 2
+    },
+    phone: {
+    	type: 'string',
+    	regex: /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
+    }
+    salary: {
+    	type: 'float',
+    	defaultsTo: 0.00
+    }
   }
+
 };
 
